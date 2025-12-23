d9f403eb0efdc85260a98a6ae50a0e7e2940524e
maria <zubakovamary@gmail.com>
2025-12-23 10:56:52 +0300

    исправлены ошибки isort в тестовом файле

--- homework03/test_maze.py
+++ homework03/test_maze.py
@@ -1,5 +1,6 @@
 import unittest
 from random import seed
+
 import maze
 
 
@@ -273,7 +274,8 @@ class MazeTest(unittest.TestCase):
         ]
         second_exit_1 = (4, 3)
         self.assertEqual(
-            [(4, 3), (3, 3), (3, 2), (3, 1), (3, 0)], maze.shortest_path(grid_1, second_exit_1)
+            [(4, 3), (3, 3), (3, 2), (3, 1), (3, 0)],
+            maze.shortest_path(grid_1, second_exit_1),
         )
 
         grid_2 = [
@@ -326,5 +328,3 @@ class MazeTest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-
